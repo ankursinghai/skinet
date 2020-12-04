4172a8dd2ae80da441e365f2585b7be7e3d2a9af
ANkur Jain <ankur.singhai@live.com>
2020-12-04 17:17:12 +0530

    Skinet changes

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        //Async Method
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+        //Sync Method
+        //public ActionResult<List<Product>> GetProducts()
+        //{
+        //   var products = _context.Products.ToList();
+        //    return products;
+        //}
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return product;
+        }
+    }
+}
