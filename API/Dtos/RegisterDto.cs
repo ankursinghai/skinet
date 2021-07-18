5a7468e183888e36d7701d7d0eb5c94636a6b872
ANkur Jain <ankur.singhai@live.com>
2021-07-18 18:32:27 +0530

    end of section 16

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", ErrorMessage =
+        "Password must have 1 upper letter, atleats 1 special character and atleast 1 number and password length should be greater or equals to 8")]
         public string Password { get; set; }
     }
 }
